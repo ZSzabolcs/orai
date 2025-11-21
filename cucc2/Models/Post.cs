dfb0899e785cb5e91ca38656f492275641b8d34b
ZSzabolcs <zelenaksz@kkszki.hu>
2025-11-21 08:39:50 +0100

    Német cigány

---
+++ cucc2/Models/Post.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace cucc2.Models
+{
+    public class Post
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [Column(TypeName = "varchar(30)")]
+        public string? Category { get; set; }
+        [Required]
+        [Column(TypeName = "longtext")]
+        public string? Description { get; set; }
+        [Required]
+        public bool Comments { get; set; }
+        public DateTime RegTime { get; set; } = DateTime.Now;
+        public DateTime ModTime { get; set; } = DateTime.Now;
+    }
+}
